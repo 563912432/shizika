da87faf3d7d9e3368a9a518b326e4c53db0aa54d
陈帅 <1301491593@qq.com>
2018-12-04 08:43:59 +0800

    'ad'

--- src/main.js
+++ src/main.js
@@ -3,16 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueCookie from 'vue-cookie'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueCookie)
 
+router.beforeEach((to, from, next) => {
+  // 统计代码
+  if (to.path) {
+    var _hmt = _hmt || []
+    _hmt.push(['_trackPageview', location.pathname + '#/' + to.fullPath])
+  }
+  next()
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
